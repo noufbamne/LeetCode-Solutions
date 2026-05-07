6fa4899792f287bc6ab2e943fdf14eaf091cee1f
Nouf Bamne <noufbamne@gmail.com>
2026-05-07 19:31:26 +0530

    Time: 136 ms (5.04%), Space: 48.6 MB (5.58%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+       
+        String rev = "";
+        for (int i = s.length() -1; i >= 0 ; i--){
+            rev = rev + s.charAt(i);
+        }
+        return s.equals(rev);
+    }
+}
