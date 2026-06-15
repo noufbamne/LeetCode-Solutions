fe8a30bc6b20063d618a4b15ed2ab0a4cbd1f10d
Nouf Bamne <noufbamne@gmail.com>
2026-06-15 19:10:49 +0530

    Time: 0 ms (100%), Space: 42.9 MB (99.03%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        int low = 0; 
+        int mid = 0; 
+        int high = nums.length -1;
+
+        while(mid <= high){
+
+        
+            if(nums[mid] == 0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+
+                low++;
+                mid++;
+            }
+
+            else if(nums[mid] == 1){
+                mid++;
+            }
+
+            else{
+                int temp1 = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp1;
+
+                high--;
+            }
+        }
+
+    }
+}
