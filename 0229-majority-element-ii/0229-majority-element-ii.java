443eabc4fd7d7de73f97c3e226ece27e14e0f6aa
Nouf Bamne <noufbamne@gmail.com>
2026-06-30 22:26:59 +0530

    Time: 3 ms (72.67%), Space: 52.6 MB (40.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,36 +1,51 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int cnt1 = 0, cnt2 = 0;
-        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
+        
+        int cnt1 =0;
+        int cnt2 = 0;
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
 
-        for(int i = 0; i < nums.length; i++) {
-            if (cnt1 == 0 && el2 != nums[i]) {
+        for(int i = 0; i < nums.length; i++){
+            if(cnt1 == 0 && nums[i] != el2){
                 cnt1 = 1;
-                el1 = nums[i]; 
-            } else if (cnt2 == 0 && el1 != nums[i]) {
+                el1 = nums[i];
+            }
+
+            else if(cnt2 == 0 && nums[i] != el1){
                 cnt2 = 1;
-                el2 = nums[i]; 
-            } else if (nums[i] == el1) {
+                el2 = nums[i];
+            }
+
+            else if(nums[i] == el1){
                 cnt1++;
-            } else if (nums[i] == el2) {
-                cnt2++; 
-            } else {
-                cnt1--; 
+            }
+
+            else if(nums[i] == el2){
+                cnt2++;
+            }
+
+            else{
+                cnt1--;
                 cnt2--;
             }
         }
 
-        cnt1 = 0; cnt2 = 0; 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == el1) cnt1++; 
-            if (nums[i] == el2) cnt2++;
+        ArrayList <Integer> list = new ArrayList<>();
+        cnt1 =0;
+        cnt2 =0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == el1){
+                cnt1++;
+            }
+            if(nums[i] == el2){
+                cnt2++;
+            }
         }
-
-        int mini = nums.length / 3 + 1;
-        List<Integer> result = new ArrayList<>(); 
-        if (cnt1 >= mini) result.add(el1);
-        if (cnt2 >= mini && el1 != el2) result.add(el2);
-
+        List <Integer> result = new ArrayList<>();
+        int mini = (nums.length/3) +1;
+        if(cnt1 >=  mini) result.add(el1);
+        if(cnt2 >= mini && el1 != el2) result.add(el2);
         return result;
     }
 }
