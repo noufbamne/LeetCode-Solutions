48517e7db420a9151e9f36b7831794932b1f2c42
Nouf Bamne <noufbamne@gmail.com>
2026-07-23 21:04:55 +0530

    Time: 1 ms (62.3%), Space: 43.3 MB (50.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,19 +3,16 @@ class Solution {
 
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < strs[0].length(); i++) {
-
-            for (int j = 1; j < strs.length; j++) {
+        for(int i = 0; i < strs[0].length(); i++){
+            for(int j = 1; j < strs.length; j++){
 
                 if(i >= strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)){
                     return result.toString();
                 }
-
             }
 
             result.append(strs[0].charAt(i));
         }
-
         return result.toString();
     }
 }
