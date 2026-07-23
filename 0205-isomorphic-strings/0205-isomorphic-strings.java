3ee4d14dab18ea8ce051d5c02802a44fa17ba5e2
Nouf Bamne <noufbamne@gmail.com>
2026-07-23 22:05:52 +0530

    Time: 7 ms (80.53%), Space: 43.8 MB (59.14%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        int [] mapST = new int[256];
+        int [] mapTS = new int[256];
+
+        Arrays.fill(mapST, -1);
+        Arrays.fill(mapTS, -1);
+
+        for(int i = 0; i < s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if(mapST[ch1] != -1 && mapST[ch1] != ch2){
+                return false;
+            }
+
+            else if(mapTS[ch2] != -1 && mapTS[ch2] != ch1){
+                return false;
+            }
+
+            mapST[ch1] = ch2;
+            mapTS[ch2] = ch1;
+        }
+        return true;
+    }
+}
