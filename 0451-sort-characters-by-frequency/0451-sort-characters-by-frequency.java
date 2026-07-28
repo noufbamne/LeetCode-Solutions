5acd8c434417a27cec1dda63c9246cdff6dee246
Nouf Bamne <noufbamne@gmail.com>
2026-07-28 20:31:44 +0530

    Time: 6 ms (93.19%), Space: 46.5 MB (78.58%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,19 +1,20 @@
 class Solution {
     public String frequencySort(String s) {
         
-        int [] freq = new int [128];
+        int [] freq = new int[128];
 
         for(int i = 0; i < s.length(); i++){
             freq[s.charAt(i)]++;
         }
 
-        StringBuilder[] bucket = new StringBuilder[s.length() +1];
-        for(int i = 0; i <128; i++){
-            if(freq[i] > 0){
+        StringBuilder [] bucket = new StringBuilder[s.length() + 1];
 
+        for(int i = 0; i < 128; i++){
+            if(freq[i] > 0){
                 if(bucket[freq[i]] == null){
-                    bucket[freq[i]] = new StringBuilder();
+                    bucket[freq[i]] =  new StringBuilder();
                 }
+
                 bucket[freq[i]].append((char) i);
             }
         }
@@ -21,17 +22,17 @@ class Solution {
         StringBuilder result = new StringBuilder();
 
         for(int f = s.length(); f >= 0; f--){
-            if(bucket[f] !=null){
-                for(int j = 0; j< bucket[f].length(); j++){
-                     char ch = bucket[f].charAt(j);
+            if(bucket[f] != null){
+                for(int j = 0; j < bucket[f].length(); j++){
+                    char ch = bucket[f].charAt(j);
 
-                    for (int k = 0; k < f; k++) {
+                    for(int k = 0; k < f; k++){
                         result.append(ch);
                     }
                 }
+
             }
         }
-
         return result.toString();
     }
 }
